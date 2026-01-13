0432b64b63677fec7d49e4905310380478b7b3e7
Jhoelber <jho.elber@hotmail.com>
2026-01-13 10:04:34 -0300

    atualizando header

---
+++ src/components/Topo.tsx
@@ -0,0 +1,12 @@
+export const Topo = () => {
+
+    return (
+        <div>
+            <header className=" text-zinc-400 p-4 ">
+                {/* https://res.cloudinary.com/dfgbasaxa/image/upload/v1714649684/images/icon.png */}
+                <img src="https://res.cloudinary.com/dfgbasaxa/image/upload/v1714649684/images/icon.png" alt="logo de jacarezinho" />
+
+            </header>
+        </div>
+    )
+}
