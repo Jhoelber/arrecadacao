84332fdf7ad60f316ae002728f518e3bbfb4d749
Jhoelber <jho.elber@hotmail.com>
2026-01-13 10:28:29 -0300

    teste para totem

--- src/components/PaginaPrincipal.tsx
+++ src/components/PaginaPrincipal.tsx
@@ -8,22 +8,22 @@ export function PaginaPrincipal() {
 
         <div>
             <Topo/>
-            <main className="grid grid-cols-1 gap-6 place-items-center md:grid-cols-2 lg:grid-cols-3 mt-10">
+            <main className="grid grid-cols-1 gap-y-16 gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3 mt-10">
                 {itens.map((item) => (
-                    <a key={item.id} href={item.href} className="w-64 rounded-2xl border p-4 shadow-sm">
-                        <div className="flex flex-col items-center gap-3  ">
+                    <a key={item.id} href={item.href} className="w-64 p-4 ">
+                        <div className="flex flex-col items-center gap-3 ">
                             <img
                                 src={item.imgUrl}
                                 alt={item.titulo}
                                 loading="lazy"
                                 decoding="async"
-                                className="h-28 w-28 object-contain"
+                                className="h-auto w-48 object-contain"
                             />
                             <div className="text-center">
                                 <div className="text-sm font-medium">{item.titulo}
 
                                 </div>
-                                {item.descricao && <div className="text-xs text-zinc-500">{item.descricao}</div>}
+                                {item.descricao && <div className="text-xs text-zinc-500 ">{item.descricao}</div>}
                             </div>
                         </div>
                     </a>
