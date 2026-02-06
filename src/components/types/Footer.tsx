3974e0585bea54e76f41f45ffa9b39f0ee56005a
Jhoelber <jho.elber@hotmail.com>
2026-02-06 13:57:53 -0300

    inserindo footer com logo

--- src/components/types/Footer.tsx
+++ src/components/types/Footer.tsx
@@ -1,7 +1,11 @@
 export const Footer = () => {
-  return (
-    <footer className="bg-green-50 w-full mt-auto py-3 text-center">
-      <h1>Desenvolvimento | Lpx</h1>
-    </footer>
-  );
+    return (
+        <footer className="w-full mt-auto py-3 px-4 flex justify-end">
+            <img
+                className="h-4 w-auto"
+                src="https://res.cloudinary.com/dfgbasaxa/image/upload/v1770396903/lpx-removebg-preview_zmtx6i.png"
+                alt="Logo Desenvolvedor"
+            />
+        </footer>
+    );
 };
